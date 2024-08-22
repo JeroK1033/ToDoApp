5c2d332a1d62b344c9b41e3896e38f42159f3544
JeroK1033 <jeronimohenao4@gmail.com>
2024-08-22 12:42:33 -0500

    Class ToDo lista

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,17 @@
-# TODO: Add code here
+class Todo:
+    def __init__(self, code_id, title, description):
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: (list[str]) = []
+    
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self, tag: str):
+        self.tags.append(tag)
+
+    def __str__(self) -> str:
+        return f"{self.code_id} - {self.title}"
+    
